e6002dd9cbaf0a5f0f114e82648f040069294110
szorsl01 <slawomir.szor@stepstone.com>
2019-06-27 22:01:03 +0200

    Initial log4j2 config and some test to see if this works

--- src/main/java/org/slaszor/poc/lambdalogger/EchoServiceLambda.java
+++ src/main/java/org/slaszor/poc/lambdalogger/EchoServiceLambda.java
@@ -17,7 +17,7 @@ public class EchoServiceLambda implements RequestHandler<APIGatewayProxyRequestE
     public RequestResponse handleRequest(APIGatewayProxyRequestEvent input, Context context) {
         String inputBody = input.getBody();
 
-        LOG.info(" !! SUCCESS !! ");
+        LOG.info(" !! SUCCESS !! - input was: " + inputBody);
 
         Map<String, String> headers = new HashMap<>();
         headers.put("version", "0.0.1");
---
+++ src/test/java/org/slaszor/poc/lambdalogger/EchoServiceLambdaTests.java
@@ -0,0 +1,33 @@
+package org.slaszor.poc.lambdalogger;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class EchoServiceLambdaTests {
+
+    @Mock
+    Context context;
+    @Mock
+    APIGatewayProxyRequestEvent requestEvent;
+
+    @Test
+    public void handleRequest() {
+        String input = "kle kle";
+
+        Mockito.when(requestEvent.getBody()).thenReturn(input);
+
+        EchoServiceLambda echoService = new EchoServiceLambda();
+        RequestResponse requestResponse = echoService.handleRequest(requestEvent, context);
+
+        assertEquals(input, requestResponse.getBody());
+
+    }
+}
