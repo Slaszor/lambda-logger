c2708b543608740fd42136f59b0fb24b3aa61776
szorsl01 <slawomir.szor@stepstone.com>
2019-06-27 21:20:30 +0200

    Spring booting the project

---
+++ src/main/java/org/slaszor/poc/lambdalogger/LambdaLoggerApplication.java
@@ -0,0 +1,13 @@
+package org.slaszor.poc.lambdalogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LambdaLoggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LambdaLoggerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/slaszor/poc/lambdalogger/LambdaLoggerApplicationTests.java
@@ -0,0 +1,16 @@
+package org.slaszor.poc.lambdalogger;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LambdaLoggerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
